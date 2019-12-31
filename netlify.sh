e67ecf9adeef0e65f3d0e25f9d824b153601c271
ZeusTheTrueGod <l-2732@yandex.ru>
2019-12-31 23:36:48 +0000

    try to deploy to google cloud

--- netlify.sh
+++ netlify.sh
@@ -1,2 +1,10 @@
 mkdir -p dist/
 cp _redirects index.html dist/
+echo DEPLOYING TO GOOGLE CLOUD
+
+echo $BASE64GOOGLETOKEN | base64 -d > /tmp/key.json
+curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-274.0.1-linux-x86_64.tar.gz > /tmp/gcloud.tar.gz
+(cd /tmp && tar zvxf gcloud.tar.gz)
+/tmp/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=/tmp/key.json --project=cncf-svg-autocrop
+alias gcloud=/tmp/google-cloud-sdk/bin/gcloud
+bash deploy.sh
