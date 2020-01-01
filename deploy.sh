be79b5ed492e34d401f6af34a64d5c00ba6dcdca
ZeusTheTrueGod <l-2732@yandex.ru>
2020-01-01 00:35:41 +0000

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 export g=`which gcloud || echo /tmp/google-cloud-sdk/bin/gcloud`
 $g functions deploy autocrop \
-    --region=us-central1
+    --region=us-central1 \
     --trigger-http \
     --runtime nodejs10 \
     --memory 2048MB \
