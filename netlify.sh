697cfe55e37e5d6ca5b02e37d3442129e85159ed
ZeusTheTrueGod <l-2732@yandex.ru>
2020-01-01 00:13:43 +0000

    turn on tar logs

--- netlify.sh
+++ netlify.sh
@@ -4,7 +4,7 @@ echo DEPLOYING TO GOOGLE CLOUD
 
 echo $BASE64GOOGLETOKEN | base64 -d > /tmp/key.json
 curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-274.0.1-linux-x86_64.tar.gz > /tmp/gcloud.tar.gz
-(cd /tmp && tar zvxf gcloud.tar.gz)
+(cd /tmp && tar zxf gcloud.tar.gz)
 /tmp/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=/tmp/key.json --project=cncf-svg-autocrop
 alias gcloud=/tmp/google-cloud-sdk/bin/gcloud
 bash deploy.sh
